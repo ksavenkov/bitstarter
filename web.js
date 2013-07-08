eeb370839384f836c795bb05ccaeb62e05c21fd3
Konstantin Savenkov <konstantin.savenkov@gmail.com>
2013-07-08 02:00:42 +0000

    read file

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var fs = require('fs')
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = new Buffer(100);
+  var fd = fs.openSync('index.html', 'r');
+  fs.readSync(fd, buffer);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
